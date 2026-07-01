e4f49074590bdabb138ed31c9775c5af79d4c5e6
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-01 20:42:13 +0300

    added callcack findStudent function

---
+++ callbacks.js
@@ -0,0 +1,13 @@
+import { students } from "./data";
+
+export function findStudent(id, callback) {
+    console.log(`-> Search for student ID: ${id} ...`);
+    setTimeout(() => {
+        const student = students.find((s) => s.id === id);
+        if (!student) {
+            callback(`Student ID: ${id} not found`, null);
+        } else {
+            callback(null, student);
+        }
+    }, 1000);
+}
