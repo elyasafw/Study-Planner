5b474296033cf2032632614f6d186a1f0c20e1b7
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-01 19:31:48 +0300

    create students and tasks

---
+++ data.js
@@ -0,0 +1,28 @@
+const students = [
+    {
+        id: 1,
+        name: "david",
+        phone: "0501234567"
+    },
+    {
+        id: 2,
+        name: "dana"
+    }
+];
+
+const tasks = [
+    {
+        title: "breakfast",
+        duration: 15
+    },
+    {
+        title: "study node.js",
+        duration: 60
+    },
+    {
+        title: "work on project",
+        duration: 120
+    }
+];
+
+module.exports = { students, tasks };
